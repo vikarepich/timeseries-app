68b6b1837de11a46abb6c5f7d767cab619ccd7d3
vikarepich <vv.repich@gmail.com>
2026-03-02 13:25:20 +0000

    initial commit: add Streamlit app

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Page configuration
+st.set_page_config(
+    page_title="Time Series Forecasting",
+    page_icon="📈",
+    layout="wide"
+)
+
+# Sample title and description
+st.title("📈 Time Series Forecasting App")
+st.write("Welcome to the Time Series Forecasting App.")
+st.info("I am saving my file here.")
